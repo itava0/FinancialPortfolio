a279fb03bc61c0ae5b9fab4a95c9ddd89906e2cb
Ivan Tavarez <ivantavarez2@gmail.com>
2023-11-14 12:03:17 -0500

    Created both interfaces

---
+++ src/main/java/com/pluralsight/finance/FixedAsset.java
@@ -0,0 +1,18 @@
+package com.pluralsight.finance;
+
+public abstract class FixedAsset implements Valuable{
+    private String name;
+    private double marketValue;
+    public FixedAsset(String name, double marketValue){
+        this.name = name;
+        this.marketValue = marketValue;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getValue() {
+        return marketValue;
+    }
+}
